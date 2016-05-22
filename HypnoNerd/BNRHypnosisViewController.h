bc4588f78e6b96f3fb7b2521208f1c7915ac41ff
Steven Liu <steven.github@gmail.com>
2016-05-21 21:41:00 -0700

    removed default controller and added customer controller

--- HypnoNerd/BNRHypnosisViewController.h
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BNRHypnosisViewController.h
 //  HypnoNerd
 //
 //  Created by Steven Liu on 2016-05-21.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BNRHypnosisViewController : UIViewController
 
 @end
-
