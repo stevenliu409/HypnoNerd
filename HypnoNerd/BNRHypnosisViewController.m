a84e3365d17ef23ca186a1cb4315c53c804063ba
Steven Liu <steven.github@gmail.com>
2016-05-23 15:40:41 -0700

    setup view in viewcontroller

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BNRHypnosisViewController.h"
+#import "BNRHypnosisView.h"
 
 @interface BNRHypnosisViewController ()
 
@@ -14,24 +15,26 @@
 
 @implementation BNRHypnosisViewController
 
-- (void)viewDidLoad {
+
+// Setting up the view programatically
+-(void)loadView
+{
+    BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view = backgroundView;
+    
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
